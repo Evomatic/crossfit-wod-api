b158cec40f7264fd86638c190e3eb5b1ac655564
Evan Trujillo <evantrujillo30@gmail.com>
2024-02-13 15:46:31 +0900

    Add versioning support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import { v1Router } from "./v1/routes";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("<h2>It's Working!</h2>");
-});
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
