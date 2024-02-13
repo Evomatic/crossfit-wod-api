d8048d6ba2cc1a7fdeedcc6a4837e40a29e30b38
Evan Trujillo <evantrujillo30@gmail.com>
2024-02-13 16:48:07 +0900

    Create service layer
    * Connected controller layer with service layer

--- src/v1/controllers/workoutController.ts
+++ src/v1/controllers/workoutController.ts
@@ -1,21 +1,34 @@
+import {
+  getAllWorkoutsService,
+  getExistingWorkoutService,
+  createNewWorkoutService,
+  updateExistingWorkoutService,
+  deleteExistingWorkoutService,
+} from "../services/workoutService";
+
 import { Request, Response } from "express";
 
 export const getAllWorkouts = (req: Request, res: Response) => {
+  const allWorkouts = getAllWorkoutsService();
   res.send("Get all workouts");
 };
 
 export const getExistingWorkout = (req: Request, res: Response) => {
+  const existingWorkout = getExistingWorkoutService();
   res.send("Get an existing workout");
 };
 
 export const createNewWorkout = (req: Request, res: Response) => {
+  const newWorkout = createNewWorkoutService();
   res.send("Create a new workout");
 };
 
 export const updateExistingWorkout = (req: Request, res: Response) => {
+  const updateWorkout = updateExistingWorkoutService();
   res.send("Update existing workout");
 };
 
 export const deleteExistingWorkout = (req: Request, res: Response) => {
+  const deleteWorkout = deleteExistingWorkoutService();
   res.send("Delete an existing workout");
 };
---
+++ src/v1/services/workoutService.ts
@@ -0,0 +1,19 @@
+export const getAllWorkoutsService = () => {
+  return;
+};
+
+export const getExistingWorkoutService = () => {
+  return;
+};
+
+export const createNewWorkoutService = () => {
+  return;
+};
+
+export const updateExistingWorkoutService = () => {
+  return;
+};
+
+export const deleteExistingWorkoutService = () => {
+  return;
+};
